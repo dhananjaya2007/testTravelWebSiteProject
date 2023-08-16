5dcd4387de0a50e1e0b4099bf65b51b9e13d0071
dhananjaya2007 <43945746+dhananjaya2007@users.noreply.github.com>
2023-08-16 12:17:14 -0400

    change done by user B

--- tests/test_login.py
+++ tests/test_login.py
@@ -25,3 +25,4 @@ class TestLogin:
         assert register_success_lbl.__contains__(username_password[0]), "User registration failed!"
         # new comment
         # comment 1 in test_login.py
+        # change done by user B
